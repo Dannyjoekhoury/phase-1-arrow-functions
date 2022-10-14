5744f5de555375f18d9bda87717a66741556fced
Danny Joe Khoury <dannyjoekhoury@gmail.com>
2022-10-14 15:13:23 -0400

    commit tree 6c527cb9c36a67f07ff6ef5371e7b99864446d15

---
+++ index.js
@@ -0,0 +1,14 @@
+let a = 2000;
+let b = 100;
+
+let divide = (a,b) => a / b;
+divide (a,b);
+
+let x = 2;
+let square = (x) => x * 2;
+square(x);
+
+let c = 4;
+let d = 3;
+let add = (c,d) => c + d;
+add(c,d);
